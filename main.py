de388a4b1c87012b0a8f928bd475cb8001f7e672
YouriLangh <neere210@hotmail.com>
2024-03-09 13:02:10 +0100

    Data imports
    Importing the CSVs and using chunks to load data in. git lfs used for  pushing large csvs to Github

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dash import Dash, html
+from src.data.preprocess import load_and_preprocess_data
 
 def main() -> None:
+    filepath = 'data/Crime_Data_from_2010_to_2019.csv'
+    data = load_and_preprocess_data(filepath)
     app = Dash()
     app.title = "Crime Dashboard"
     app.layout = html.Div(children="Hello World!")
